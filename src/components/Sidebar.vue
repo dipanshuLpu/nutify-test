97e9855cdabca909269f740990d0ae04b864fd7a
Dipanshu Bhangale <dipanshubhangale@WFX-LAP-961.local>
2022-09-08 23:36:14 +0530

    initial commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-img
+      height="140"
+      class="pa-4"
+      src="https://preview.pixlr.com/images/800wm/1439/2/1439104804.jpg"
+    >
+      <div class="text-center">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <v-img
+            aspect-ratio="30"
+            src="https://yt3.ggpht.com/esazPAO03T0f0vKdByJvkDy6MSwjyG5f-c_2S2CJapszQ3KPQyZarpoqvgv0Us0atUbILytj=s88-c-k-c0x00ffffff-no-rj"
+          />
+        </v-avatar>
+        <h2 class="white--text">Web Burden</h2>
+      </div>
+    </v-img>
+    <v-divider></v-divider>
+    <template>
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="(item,i) in 4"
+      :key="i"
+    >
+      <v-expansion-panel-header>
+        Item
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+      <v-expansion-panel-content>
+       equat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: ["drawer"],
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard"],
+        ["mdi-account", "Profile"],
+        ["mdi-clipboard-list-outline", "Products"],
+        ["mdi-card-account-details-outline", "Orders"],
+        ["mdi-cog", "System Setting"],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <v-alert
+        v-model="alert"
+        dismissible
+        color="cyan"
+        border="left"
+        elevation="2"
+        colored-border
+        icon="mdi-twitter"
+      >
+        You've got <strong>5</strong> new updates on your timeline!.
+      </v-alert>
+  
+      <div class="text-center">
+        
+      </div>
+    </div>
+  </template>
+
+<script>
+    export default {
+      data () {
+        return {
+          alert: true,
+        }
+      },
+    }
+  </script>
